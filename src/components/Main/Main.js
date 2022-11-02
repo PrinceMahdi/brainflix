c1ca507ab90b502e9d442dc74af4a09a5358de8f
Mahdi Shirmohammadi <shirmohammadi2000@gmail.com>
2022-11-01 19:36:28 -0700

    currently playing video has been made for all devices

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import "./Main.scss";
+import Video from "../Video/Video";
+
+const Main = () => {
+  return (
+    <>
+      <main>
+        <Video />
+      </main>
+    </>
+  );
+};
+
+export default Main;
