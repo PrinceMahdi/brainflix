7c61458c88fb41174c7100c2ed74df27afa82ed3
Mahdi Shirmohammadi <shirmohammadi2000@gmail.com>
2022-11-17 23:43:59 -0800

    implementing the diving deeper section

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -3,36 +3,61 @@ import "./Comment.scss";
 // <------------- COMPONENT IMPORTS ------------->
 import CommentCard from "../CommentCard/CommentCard";
 
-const Comment = ({ videoDetails }) => {
+// <------------- REACT IMPORTS ------------->
+import axios from "axios";
+
+const Comment = ({ videoDetails, setVideoDetails, BACKEND_URL }) => {
   // Adding the length of comments if it's not undefined
   let commentLength = videoDetails.comments?.length;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("ftest");
+    if (!e.target.userComment.value) {
+      alert("Please type something before proceeding...");
+    } else {
+      let newComment = {
+        comment: e.target.userComment.value,
+        likes: 0,
+        timestamp: Date.now(),
+      };
+      axios
+        .post(`${BACKEND_URL}/videos/${videoDetails.id}/comments`, newComment)
+        .then((response) => {
+          setVideoDetails(response.data);
+        });
+      e.target.reset();
+    }
+  };
 
   return (
     <div className="comment">
       <h5 className="comment__title">{commentLength} Comments</h5>
       <article className="comment__form-container">
         <div className="comment__form-icon"></div>
-        <form className="comment__form" id="form">
-          <label htmlFor="comment" className="comment__form__label">
-            JOIN THE CONVERSATION
-          </label>
-          <textarea
-            className="comment__form__input"
-            placeholder="Add a new comment"
-            name="userComment"
-            id="comment"
-            cols="20"
-            rows="5"
-          ></textarea>
+        <form className="comment__form" id="form" onSubmit={handleSubmit}>
+          <div className="comment__form-container--top">
+            <label htmlFor="comment" className="comment__form__label">
+              JOIN THE CONVERSATION
+            </label>
+            <textarea
+              className="comment__form__input"
+              placeholder="Add a new comment"
+              name="userComment"
+              id="comment"
+              cols="20"
+              rows="5"
+            ></textarea>
+          </div>
+
           <div className="comment__form-button-container-mobile">
-            <div className="comment__form__button-icon-mobile"></div>
+            <button className="comment__form-button">COMMENT</button>
+          </div>
+          <div className="comment__form-button-container">
             <button className="comment__form-button">COMMENT</button>
           </div>
         </form>
-        <div className="comment__form-button-container">
-          <div className="comment__form__button-icon"></div>
-          <button className="comment__form-button">COMMENT</button>
-        </div>
       </article>
       {/* Mapping throught the comments array only if it's not undefined */}
       {videoDetails.comments?.map((data) => (
